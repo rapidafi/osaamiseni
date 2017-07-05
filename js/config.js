51cf8e44bc400e869177e11d1f774c5597ebeb69
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-07-05 14:19:28 +0300

    Start using secure connection to Koodistopalvelu.

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ let lang='fi';
 let baseuri=location.origin;
 let osaamiseniuri=baseuri+'';
 
-let opintopolkuuri = "http://koodistopalvelu.fi/opintopolku.php?type=json";
+let opintopolkuuri = "https://koodistopalvelu.fi/opintopolku.php?type=json";
 
 if (location.hostname=='127.0.0.1' || location.hostname=='localhost') {
   console.log("@localhost: developing")
